13865abeabe801a6b8273ba9e635b1896c3886fa
Cucorn <zeromassmedia@gmail.com>
2026-05-01 11:09:11 -0400

    Add task definitions for Assignment LID plugin
    Define task for Assignment LID plugin to process queue every 5 minutes.

---
+++ db/tasks.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Task definitions for Assignment LID plugin.
+ *
+ * @package    assignsubmission_lid
+ * @copyright  2026 Sean Rugg
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$tasks = [
+    [
+        'classname' => 'assignsubmission_lid\task\process_queue',
+        'blocking' => 0,
+        'minute' => '*/5', // Run every 5 minutes.
+        'hour' => '*',
+        'day' => '*',
+        'month' => '*',
+        'dayofweek' => '*',
+    ],
+];
